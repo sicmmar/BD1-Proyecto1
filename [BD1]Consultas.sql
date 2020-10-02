bba4650ad0a6d193fdcd4d57a724f2927e54a63e
sicmmar <sicmariana8@gmail.com>
2020-10-01 19:51:42 -0600

    CONSULTA 1

--- [BD1]Consultas.sql
+++ [BD1]Consultas.sql
@@ -1,3 +1,7 @@
 --Script (con extensión .sql) que contiene todas las consultas necesarias para realizar
 --los reportes.
 
+select persona.nombre, persona.telefono, transaccion.cod_transaccion, transaccion.precio_total from transaccion, persona 
+where transaccion.cod_persona = persona.cod_persona and transaccion.precio_total = (select max(precio_total) from transaccion) and persona.cod_rol = 2;
+
+
